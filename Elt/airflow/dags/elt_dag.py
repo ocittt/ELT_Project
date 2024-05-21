337e9948a616300a2b95cbbcf2a3d79e001c5d87
Imtiyaz <63872669+ocittt@users.noreply.github.com>
2024-05-21 17:24:19 +0700

    on Going Airbythe

--- Elt/airflow/dags/elt_dag.py
+++ Elt/airflow/dags/elt_dag.py
@@ -1,12 +1,15 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from docker.types import Mount
+from airflow.utils.dates import days_ago
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash import BashOperator
+
 
 from airflow.providers.docker.operators.docker import DockerOperator
 import subprocess
+
+CONN_ID= ''
 
 
 default_args ={
@@ -16,14 +19,7 @@ default_args ={
     'email_on_retry': False,  
 }
 
-def run_elt_script():
-    script_path = "/opt/airflow/elt/elt_script.py"
-    result = subprocess.run(["python", script_path],
-                            capture_output=True, text=True)
-    if result.returncode != 0:
-        raise Exception(f"Script failed with error: {result.stderr}")
-    else:
-        print(result.stdout)
+
         
 dag = DAG(
     'elt_and_dbt',
@@ -34,10 +30,14 @@ dag = DAG(
 )
 
 
-t1 = PythonOperator(
-    task_id='run_elt_script',
-    python_callable=run_elt_script,
-    dag=dag,
+t1 = AirbyteTriggerSyncOperator(
+    task_id='airbyte_postgres_postgres',
+    airbyte_conn_id='airbyte',
+    connection_id=CONN_ID,
+    asynchronous=False,
+    timeout=3600,
+    wait_seconds=3,
+    dag=dag
 )
 
 t2 = DockerOperator(
